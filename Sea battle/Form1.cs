971d7dc0c8c4f5329eaa77b69c356adfb413bb57
Illia <kroshka.illya@lll.kpi.ua>
2023-12-20 22:00:50 +0200

    feature: implement maps generation

--- Sea battle/Form1.cs
+++ Sea battle/Form1.cs
@@ -1,3 +1,6 @@
+using System.CodeDom;
+using System.Windows.Forms;
+
 namespace Sea_battle
 {
     public partial class Form1 : Form
@@ -5,6 +8,42 @@ namespace Sea_battle
         public Form1()
         {
             InitializeComponent();
+            firstMap = Map.createMapArray(mapSize);
+            secondMap = Map.createMapArray(mapSize);
+            createMaps();
+        }
+        private const int mapSize = 10;
+        private int cellSize = 30;
+        private int[,] firstMap;
+        private int[,] secondMap;
+        private bool isPlaying = false;
+        
+        
+        private void createMaps()
+        {
+            this.Width = mapSize * 2 * cellSize + 70;
+            this.Height = (mapSize + 1) * cellSize + 20;
+            Map.createMaps(mapSize,cellSize, firstMap, secondMap, this.Controls);
+
+            Button startButton = new Button();
+            startButton.Text = "Start";
+            startButton.Click += new EventHandler(Start);
+            startButton.Location = new Point(0, mapSize * cellSize + 20);
+            this.Controls.Add(startButton);
+        }
+
+        private void Start(object sender, EventArgs e)
+        {
+            isPlaying = true;
+        }
+
+        private void ConfigureShips(object sender, EventArgs e)
+        {
+            Button pressedButton = sender as Button;
+            if (isPlaying)
+            {
+                pressedButton.BackColor = Color.Red;
+            }
         }
     }
 }
---
+++ Sea battle/Map.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sea_battle
+{
+    internal class Map
+    {
+        public static int[,] createMapArray(int mapSize)
+        {
+            int[,] Map = new int[mapSize, mapSize];
+            return Map;
+        }
+
+        public void sayHello()
+        {
+
+        }
+
+        private static void PlayerShoot(object sender, EventArgs e, int[,] enemyMap)
+        {
+            Button pressedButton = sender as Button;
+            Shoot(enemyMap, 30, pressedButton);
+        }
+
+        private static bool Shoot(int[,] map,int cellSize ,Button pressedButton)
+        {
+            bool hit = false;
+
+            int delta = 0;
+            if (pressedButton.Location.X > 320)
+            {
+                delta = 320;
+            }
+            if (map[pressedButton.Location.Y / cellSize, (pressedButton.Location.X - delta)/ cellSize] != 0)
+            {
+                hit = true;
+                pressedButton.BackColor = Color.Blue;
+                pressedButton.Text = "X";
+            }
+            else
+            {
+                hit = false;
+
+                pressedButton.BackColor = Color.Black;
+            }
+            return hit;
+        }
+
+        private static void ConfigureShips(object sender, EventArgs e, int[,] map, int cellSize)
+        {
+            Button pressedButton = sender as Button;
+            pressedButton.BackColor = Color.Red;
+            
+            if (map[pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize] == 0)
+            {
+                pressedButton.BackColor = Color.Red;
+                map[pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize] = 1;
+            }
+            else
+            {
+                pressedButton.BackColor = Color.White;
+                map[pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize] = 0;
+            }
+        }
+
+        public static void createMaps(int mapSize,int cellSize, int[,] firstMap, int[,] secondMap, Control.ControlCollection control)
+        {
+            string alphabet = "ABCDEFGHIJ";
+
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    firstMap[i, j] = 0;
+                    Button button = new Button();
+                    button.Location = new Point(j * cellSize, i * cellSize);
+                    button.Size = new Size(cellSize, cellSize);
+                    button.BackColor = Color.White;
+                    if (i == 0 || j == 0)
+                    {
+                        button.BackColor = Color.Gray;
+                        if (i == 0 && j>0)
+                        {
+                            button.Text = alphabet[j - 1].ToString();
+                        }
+                        if (j == 0 && i > 0)
+                        {
+                            button.Text = i.ToString();
+                        }
+                    }
+                    else
+                    {
+                        button.Click += new EventHandler((sender, e) => ConfigureShips(sender, e, secondMap, cellSize));
+                    }
+                    control.Add(button);
+                }
+            }
+
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    secondMap[i, j] = 0;
+                    Button button = new Button();
+                    button.Location = new Point(350 + j * cellSize, i * cellSize);
+                    button.Size = new Size(cellSize, cellSize);
+                    button.BackColor = Color.White;
+                    if (i == 0 || j == 0)
+                    {
+                        button.BackColor = Color.Gray;
+                        if (i == 0 && j > 0)
+                        {
+                            button.Text = alphabet[j - 1].ToString();
+                        }
+                        if (j == 0 && i > 0)
+                        {
+                            button.Text = i.ToString();
+                        }
+                    }
+                    else
+                    {
+                        button.Click += new EventHandler((sender, e) => PlayerShoot(sender, e, secondMap));
+                    }
+                    control.Add(button);
+                }
+            }
+        }
+    }
+}
